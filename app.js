8fc73820523802e44551f42831d6e4c52d3be5ab
Elena Ristea <Elena.Ristea@nology.io>
2022-12-01 12:58:06 +0000

    added card functions

---
+++ app.js
@@ -0,0 +1,67 @@
+import { cards } from "./data/cardsData.js";
+console.log(cards);
+
+const cardContainer = document.querySelector(".card-container");
+const heartsButton = document.getElementById("hearts-button");
+const allCardsButton = document.getElementById("all-cards-button");
+const cardColourBlack = document.getElementsByClassName(".card--black");
+const cardColourRed = document.getElementsByClassName(".card--red");
+
+const cardSuit = cards.map((element) => element.suit);
+const cardColour = cards.map((element) => element.colour);
+const cardValue = cards.map((element) => element.value);
+const cardSymbol = cards.map((element) => element.symbol);
+
+// Create a card
+
+const createCardBlack = (suit, value, symbol) => {
+  let htmlCardBlack = `
+    <div class="card-container">
+        <div class="card">
+            <div class="card--black">
+                <h3 class="card__heading">
+                ${value} of ${symbol}
+                </h3>
+                <p class="card__description">
+                The ${value} of ${suit}
+                </p>
+            </div>
+        </div>
+    </div>`;
+  return htmlCardBlack;
+};
+
+const createCardRed = (suit, value, symbol) => {
+  let htmlCardRed = `
+    <div class="card-container">
+        <div class="card">
+            <div class="card--red">
+                <h3 class="card__heading">
+                ${value} of ${symbol}
+                </h3>
+                <p class="card__description">
+                The ${value} of ${suit}
+                </p>
+            </div>
+        </div>
+    </div>`;
+  return htmlCardRed;
+};
+
+for (let index = 0; index < cards.length; index++) {
+  const suit = cardSuit[index];
+  const value = cardValue[index];
+  const symbol = cardSymbol[index];
+
+  cardContainer.innerHTML += createCardBlack(suit, value, symbol);
+  cardContainer.innerHTML += createCardRed(suit, value, symbol);
+}
+
+const cardBlack = (black) => {
+  if ((cardColour = cardColourBlack)) {
+    return cardBlack;
+  } else {
+    cardColour = cardColourRed;
+    return cardColourRed;
+  }
+};
